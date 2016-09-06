c916e9b8874020a3ed1436ec43706929a16c2cba
JimboWorks <james_rock96@yahoo.com>
2016-09-06 21:40:12 +0800

    blog

--- javascript/function.js
+++ javascript/function.js
@@ -29,20 +29,20 @@
 **/
 
 $(function(){
-    $(".infi-scroll .objects").slice(0, 3).show().css('display', 'inline-block'); // select the first five
+    $(".infi-scroll .objects").slice(0, 3).show('slow').css('display', 'inline-block'); // select the first five
 
     $("#load").click(function(e){ // click event for load more
         e.preventDefault();
-        $(".infi-scroll .objects:hidden").slice(0, 3).show().css('display', 'inline-block'); // select next 3 hidden divs and show them
+        $(".infi-scroll .objects:hidden").slice(0, 3).fadeIn('slow').css('display', 'inline-block'); // select next 3 hidden divs and show them
         if($(".infi-scroll .objects:hidden").length == 0){ // check if any hidden divs still exist
             $('#load a').hide();
-            $('#hide').show();
+            $('#hide').fadeIn('slow');
         }
     });
 
     $("#hide").click(function(event) { // click event for show less posts
       $(".infi-scroll .objects").slice(3, 10).hide();
-      $("#load a").text('View More').show();
+      $("#load a").text('View More').fadeIn('slow');
       $(this).hide();
     });
 });
